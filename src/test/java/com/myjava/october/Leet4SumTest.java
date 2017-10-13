33375554b55d91873953f3065693d0646f7bfb88
xionghekuan <xionghekuan@4417.com>
2017-10-13 10:49:05 +0800

    垃圾win,搞了一个多小时,终于把junit的依赖弄好了

---
+++ src/test/java/com/myjava/october/Leet4SumTest.java
@@ -0,0 +1,33 @@
+package com.myjava.october;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+
+/**
+ * Leet4Sum Tester.
+ *
+ * @author <Authors name>
+ * @version 1.0
+ * @since <pre>ʮ�� 13, 2017</pre>
+ */
+public class Leet4SumTest {
+
+    @Before
+    public void before() throws Exception {
+    }
+
+    @After
+    public void after() throws Exception {
+    }
+
+    /**
+     * Method: fourSum(int[] num, int target)
+     */
+    @Test
+    public void testFourSum() throws Exception {
+        //TODO: Test goes here...
+        System.out.println("xhk");
+    }
+
+} 
